4f01cc2cb4f5c07cd5bca71ef9731a93f8384254
kevin-salamon <kevinwebdevs@gmail.com>
2020-07-27 12:59:45 -0400

    updated actions

--- client/src/components/List/ListActions.js
+++ client/src/components/List/ListActions.js
@@ -1,4 +1,4 @@
-import { GET_ITEMS, ADD_ITEM, UPDATE_ITEM, DELETE_ITEM } from '../../types/Types';
+import { GET_ITEMS } from '../../types/Types';
 import { getSavedWorkouts } from "../../utils/API";
 
 
